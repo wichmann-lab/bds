7d5889b8f668b483eddf08dcdf092e97b5f4e8e9
Bernhard Lang <bernhard.lang@student.uni-tuebingen.de>
2019-02-26 14:51:11 +0100

    Implemented second test of Wood (2006) p. 116.

--- python/bds/DifferenceScale.py
+++ python/bds/DifferenceScale.py
@@ -31,79 +31,159 @@ class DifferenceScale:
 
     self.precision = stanfit.summary(pars=['precision'], probs=[0.025, 0.25, 0.5, 0.75, 0.975])['summary']
 
-  def ppc(self):
+  def ppc_ordered_residuals(self):
     rhat_pars = ['resp_hat[%d]' % x for x in range(1,self.n+1)]
     ll_pars =   ['log_lik[%d]' % x for x in range(1,self.n+1)]
     pp = (self.stanfit.to_dataframe(pars=rhat_pars,
                                    inc_warmup=False,
                                    diagnostics=False)
           >> select(rhat_pars)
-         ).T
+         ).T.values
     ll = (self.stanfit.to_dataframe(pars=ll_pars, 
                                   inc_warmup=False,
                                   diagnostics=False)
           >> select(ll_pars)
-         ).T
+         ).T.values
 
-    ppcols = ['smp[%d]' % x for x in range(0, pp.shape[1])]
-    llcols = ['smp[%d]' % x for x in range(0, pp.shape[1])]
-    pp.columns = ppcols
-    ll.columns = llcols
+    resp = self.data['Response']
 
-    pp.loc[:,'trial'] = np.arange(1, self.n+1)
-#    ll.loc[:,'trial'] = np.arange(1, self.n+1)
+    emp_resid = (2.0*np.tile(resp[:, np.newaxis], (1,pp.shape[1]))-1.0) * np.sqrt(-2.0 * ll)
+    sim_resid = (2.0*pp-1.0) * np.sqrt(-2.0 * ll)
 
-    ll >>= gather('smp', 'll')
+    emp_resid_sorted = np.sort(emp_resid, axis=0)
+    sim_resid_sorted = np.sort(sim_resid, axis=0)
 
-    ppc = self.data.set_index('trial') >> bind_cols(pp.set_index('trial'))
+    cols = ['smp[%d]' % x for x in range(0, pp.shape[1])]
+    emp_df = pd.DataFrame(data = emp_resid_sorted,
+                          columns = cols)
 
-    ppc = ppc.reset_index()
-    ppc >>= gather('smp', 'rhat', ppcols)
+    emp_df['origin'] = 'empirical'
+    sim_df = pd.DataFrame(data = sim_resid_sorted,
+                          columns = cols)
+    sim_df['origin'] = 'simulated'
 
-    ppc.loc[:,'ll'] = ll.loc[:,'ll']
+    resids_df = emp_df >> bind_rows(sim_df)
+    resids_df['sortid'] = resids_df.index
+    resids_df >>= gather('smp', 'residuals', cols) 
 
-    # make np.sqrt usable with dfply's placeholder variables
-    @make_symbolic
-    def np_sqrt(a):
-      return np.sqrt(a)
-    ppc >>= mutate(dev_res=(2.0*X.Response-1.0) * np_sqrt(-2.0 * X.ll),
-                   dev_res_pred=(2.0*X.rhat-1.0) * np_sqrt(-2.0 * X.ll))
-         
     def p250(x):
-      return np.percentile(x,2.5)
+      return np.percentile(x, 2.5)
 
     def p975(x):
       return np.percentile(x, 97.5)
 
-    ppc_summary = (ppc >> group_by('trial')
+    ppc_summary = (resids_df >> group_by('sortid', 'origin')
                        >> summarize_each([p250, p975, np.median],
-                                         X.dev_res, X.dev_res_pred)
-                       >> select(X.dev_res_median, X.dev_res_pred_median,
-                                 X.dev_res_p250, X.dev_res_pred_p250,
-                                 X.dev_res_p975, X.dev_res_pred_p975)
+                                         X.residuals)
                   )
 
-    ppc_summary >>= arrange(X.dev_res_median)
+    @make_symbolic
+    def is_disjoint(emp_low, emp_high, sim_low, sim_high):
+      return np.logical_or(np.less(emp_high, sim_low),
+                           np.less(sim_high, emp_low))
 
-    ppc_summary = ppc_summary.apply(np.sort)
+    pval = (ppc_summary >> mask(X.origin == 'empirical')
+            >> inner_join(ppc_summary >> mask(X.origin == 'simulated')
+                          >> rename(sim_p250 = X.residuals_p250,
+                                    sim_p975 = X.residuals_p975)
+                          >> select(X.sim_p250, X.sim_p975, X.sortid),
+                         by='sortid')
+            >> transmute(disjoint = is_disjoint(X.residuals_p250, X.residuals_p975,
+                                                  X.sim_p250, X.sim_p975))
+            >> summarize(disjoint_sum = X.disjoint.sum())
+           )['disjoint_sum']/ppc_summary.shape[0]
 
-    # create a column with the sorted order
-    ppc_summary.reset_index(drop=True, inplace=True)
-    ppc_summary['sorted'] = ppc_summary.index
+    print('Proportion of non-overlapping intervals:', pval[0])
+
+    return (pval[0], ppc_summary)
+
+  def ppc_residuals_run(self):
+    rhat_pars = ['resp_hat[%d]' % x for x in range(1,self.n+1)]
+    ll_pars =   ['log_lik[%d]' % x for x in range(1,self.n+1)]
+    dd_pars =   ['decision[%d]' % x for x in range(1,self.n+1)]
+    pp = (self.stanfit.to_dataframe(pars=rhat_pars,
+                                   inc_warmup=False,
+                                   diagnostics=False)
+          >> select(rhat_pars)
+         ).T.values
+    ll = (self.stanfit.to_dataframe(pars=ll_pars, 
+                                  inc_warmup=False,
+                                  diagnostics=False)
+          >> select(ll_pars)
+         ).T.values
+    dd = (self.stanfit.to_dataframe(pars=dd_pars, 
+                                  inc_warmup=False,
+                                  diagnostics=False)
+          >> select(dd_pars)
+         ).T.values
+
+    resp = self.data['Response']
+
+    emp_resid = (2.0*np.tile(resp[:, np.newaxis], (1,pp.shape[1]))-1.0) * np.sqrt(-2.0 * ll)
+    sim_resid = (2.0*pp-1.0) * np.sqrt(-2.0 * ll)
+
+    def count_revs(a):
+      return np.sum(np.abs(np.diff(0.5 * np.sign(a) + 1.0, axis=0)), axis=0)
+
+    def to_bin_sign(a):
+      return 0.5 * np.sign(a) + 0.5
+
+    for i in range(0, dd.shape[1]):
+      sort_index = np.argsort(dd[:,i])
+
+      emp_resid[:,i] = emp_resid[sort_index, i]
+      sim_resid[:,i] = sim_resid[sort_index, i]
+
+    emp_resid = to_bin_sign(emp_resid)
+    sim_resid = to_bin_sign(sim_resid)
+
+    emp_resid = count_revs(emp_resid)
+    sim_resid = count_revs(sim_resid)
+
+    print(emp_resid.shape)
+    run_df = (pd.DataFrame({'origin': 'empirical', 'runs': emp_resid})
+              >> bind_rows(pd.DataFrame({'origin': 'simulated', 'runs': sim_resid}))
+             )
+    pval = np.sum(to_bin_sign(emp_resid - sim_resid))/sim_resid.shape[0]
+
+    print('Proportion of runs larger in empirical data:', pval)
+
+    return (pval, run_df)
+
+  def ppc_flip_count(self):
+    rhat_pars = ['resp_hat[%d]' % x for x in range(1,self.n+1)]
+    pp = (self.stanfit.to_dataframe(pars=rhat_pars,
+                                   inc_warmup=False,
+                                   diagnostics=False)
+          >> select(rhat_pars)
+         ).T.values
+    cols = ['smp[%d]' % x for x in range(0, pp.shape[1])]
 
     def count_revs(a):
       return np.sum(np.abs(np.diff(a)))
 
-    reversals = (ppc >> arrange(X.S1, X.S2, X.S3)
-                     >> group_by(X.smp)
-                     >> summarize_each([count_revs],X.rhat)
+    rhat_df = pd.DataFrame(data = pp,
+                           columns = cols)
+
+    reversals = (self.data
+                 >> bind_cols(rhat_df)
+                 >> gather('smp', 'rhat', cols)
+                 >> arrange(X.S1, X.S2, X.S3)
+                 >> group_by(X.smp)
+                 >> summarize_each([count_revs],X.rhat)
+                 >> ungroup()
+                 >> arrange(X.rhat_count_revs)
                 )
+
     sorted_resp = self.data >> arrange(X.S1, X.S2, X.S3)
 
-    print(sorted_resp)
-
     emp_rev = count_revs(sorted_resp['Response'])
-    return (ppc_summary, reversals, emp_rev)
+
+    pval = np.searchsorted(reversals['rhat_count_revs'], emp_rev)/reversals.shape[0]
+
+    print('Number of reversals:', emp_rev, '-- p-value:', pval[0])
+
+    return (pval[0], reversals, emp_rev)
 
   def get_scale_values(self):
     return self.scale[:,0]
@@ -120,19 +200,18 @@ class DifferenceScale:
            "\nprecision:\t" + str(self.precision[:,0]))
 
   def ppc_plot(self):
-    (ppc_df, rev_df, emp_rev) = self.ppc()
+    (_, ppc_df) = self.ppc_ordered_residuals()
+    (_, rev_df, emp_rev) = self.ppc_flip_count()
+    (_, run_df) = self.ppc_residuals_run()
 
-    print(rev_df)
-    print('empirical reversal', emp_rev)
-    return (gg.ggplot(ppc_df, gg.aes(x='dev_res_median', y='sorted')) +
-       gg.geom_line(color='red') +
-       gg.geom_line(gg.aes(x='dev_res_p250'), linetype='dashed', color='red') + 
-       gg.geom_line(gg.aes(x='dev_res_p975'), linetype='dashed', color='red') +
-       gg.geom_line(gg.aes(x='dev_res_pred_p250'), linetype='dashed', color='black') +
-       gg.geom_line(gg.aes(x='dev_res_pred_p975'), linetype='dashed', color='black') +
-       gg.geom_line(gg.aes(x='dev_res_pred_median'), linetype='solid', color='orange'),
+    return (gg.ggplot(ppc_df, gg.aes(x='residuals_median', y='sortid', color='origin')) +
+       gg.geom_line() +
+       gg.geom_line(gg.aes(x='residuals_p250')) + 
+       gg.geom_line(gg.aes(x='residuals_p975')),
        gg.ggplot(rev_df, gg.aes(x='rhat_count_revs')) +
-       gg.geom_histogram() + gg.geom_vline(gg.aes(xintercept=emp_rev))
+       gg.geom_histogram() + gg.geom_vline(gg.aes(xintercept=emp_rev)),
+       gg.ggplot(run_df, gg.aes(x='runs', fill='origin')) +
+       gg.geom_histogram()
     )
 
 class LpsDifferenceScale(DifferenceScale):
